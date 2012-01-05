26de72a172c2495facdf764df7950dba625f0fae
salvatore <salvatore@rezo.net>
2012-01-05 08:09:24 +0000

    langues (paquet-images)

--- lang/paquet-images_fr.php
+++ lang/paquet-images_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-images
-// Langue: fr
-// Date: 30-07-2011 15:01:37
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/filtres_images/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// I
+	// I
 	'images_description' => 'Filtres de transformation d\'image et de couleurs',
-	'images_slogan' => 'Filtres de transformation d\'image et de couleurs',
+	'images_slogan' => 'Filtres de transformation d\'image et de couleurs'
 );
+
 ?>

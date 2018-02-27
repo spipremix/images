755632e92e01466321be360911edf7180dc95cbc
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-27 04:36:32 +0000

    [Salvatore] [source:_core_/plugins/filtres_images/lang/ paquet-images] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-images_br.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-images?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'images_description' => 'Siloù treuzfurmadur skeudennoù ha livioù',
+	'images_slogan' => 'Siloù treuzfurmadur skeudennoù ha livioù'
+);

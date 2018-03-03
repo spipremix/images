ee8f392961c5cfac4e781648d235314960206057
gpl <gpl@long-player.net>
2018-03-03 04:37:15 +0000

    [Salvatore] [source:_core_/plugins/filtres_images/lang/ paquet-images] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-images_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-images?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'images_description' => 'Filtriloj de bild- kaj kolor-transformo',
+	'images_slogan' => 'Filtriloj de bild- kaj kolor-transformo'
+);
